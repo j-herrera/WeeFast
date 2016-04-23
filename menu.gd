f882cc76d15b47a9e90cfe43fa3f700ef43ac3d4
Javier Herrera <j.herrera.montojo@gmail.com>
2016-04-23 21:40:21 +0100

    Preliminary menu screen added

---
+++ menu.gd
@@ -0,0 +1,16 @@
+
+extends Node2D
+
+func _ready():
+	pass
+
+func _on_single_pressed():
+	get_tree().change_scene("res://main.scn")
+
+
+func _on_multi_pressed():
+	get_tree().change_scene("res://multi.scn")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
