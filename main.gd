4ee14838c669235c1c9d4e01e99f7870b47b9911
Dreyrden <rjgarner92@gmail.com>
2016-04-23 16:33:03 +0100

    added player scene for player logic, continent counters for CO2 rate

--- main.gd
+++ main.gd
@@ -1,19 +1,39 @@
 
 extends Node2D
 
-var C02_val = 10
 
+var CO2_val = 10
+
+# Define CO2 rates
+var africa_CO2_rate = 1
+var europe_CO2_rate = 1
+var north_america_CO2_rate = 1
+var south_america_CO2_rate = 1
+var asia_CO2_rate = 1
+var australasia_CO2_rate = 1
+
+func _init():
+    	var europe = continent.new()
 
 func _process(delta):
-	C02_val = C02_val + 0.1*delta
-	get_node("C02_value").set_text(str(C02_val))
+	var total_CO2_rate = africa_CO2_rate + europe_CO2_rate
+	CO2_val = CO2_val + total_CO2_rate*delta
 	
-	if C02_val > 11:
+	get_node("CO2_value").set_text(str(CO2_val))
+	
+	print(europe.CO2_rate)
+	
+	if CO2_val > 100:
 		get_tree().change_scene("res://lose.scn")
-			
-
-
+	
 func _ready():
 	set_process(true)	
+	
+func _on_reduce_pressed():
+	CO2_val = CO2_val - 1
+
+
+
+
 
 
