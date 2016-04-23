e8a983a7011894cc20082fbb7072b9527ab52d6e
federico-toso <federico.toso@strath.ac.uk>
2016-04-23 16:31:15 +0000

    introduction of time and temperature

--- main.gd
+++ main.gd
@@ -1,19 +1,28 @@
 
 extends Node2D
 
-var C02_val = 10
+var CO2_val = 10
+var temp_val = 20
+var time_val = 0
 
 
 func _process(delta):
-	C02_val = C02_val + 0.1*delta
-	get_node("C02_value").set_text(str(C02_val))
+	CO2_val = CO2_val + 0.1*delta
+	temp_val = temp_val + delta*CO2_val*0.05
+	time_val = time_val + delta
+	get_node("C02_value").set_text(str(CO2_val))
+	get_node("Temp_value").set_text(str(temp_val))
+	get_node("time_value").set_text(str(time_val))
 	
-	if C02_val > 11:
+	if temp_val > 25:
 		get_tree().change_scene("res://lose.scn")
-			
-
+	elif time_val > 25:
+		get_tree().change_scene("res://win.scn")
 
 func _ready():
 	set_process(true)	
 
-
+func _on_reduce_CO2_pressed():
+	CO2_val = CO2_val - 5
+	if CO2_val < 0:
+		CO2_val = 0
