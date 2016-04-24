6dd32f3f4098fac3bd919bd6d2e38c3f4900b4f7
Javier Herrera <j.herrera.montojo@gmail.com>
2016-04-24 11:49:04 +0100

    Unmessed server

--- server/demo.py
+++ server/demo.py
@@ -32,39 +32,6 @@ while True:
 
     # Game
     nm = 0
-    while True:
-        data, addr = sock_in.recvfrom(1024)
-
-    print(data)
-    print(addr[0])
-
-    if (addr[0] == ip1):
-        data1 = data
-        nm += 1
-
-    if (addr[0] == ip2):
-        data2 = data
-        nm += 1
-
-    if nm == 2:  # Lobby
-        ip1 = ""
-        ip2 = ""
-        msg = "Match!"
-        while True:
-            data, addr = sock_in.recvfrom(1024)
-            print(addr[0])
-            if (ip1 == ""):
-                ip1 = addr[0]
-            elif (ip1 != addr[0]):
-                ip2 = addr[0]
-                sock_out.sendto(msg.encode(encoding='UTF-8',
-                                           errors='strict'), (ip1, UDP_PORT_OUT))
-                sock_out.sendto(msg.encode(encoding='UTF-8',
-                                    errors='strict'), (ip2, UDP_PORT_OUT))
-        break
-
-    # Game
-    nm = 0
 
     current_time = time.clock()
     while True:
@@ -83,9 +50,6 @@ while True:
             nm += 1
 
         if nm == 2:
-            sock_out.sendto(data1, (ip2, UDP_PORT_OUT))
-            sock_out.sendto(data2, (ip1, UDP_PORT_OUT))
-            nm = 0
             sock_out.sendto(data1, (ip2, UDP_PORT_OUT))
             sock_out.sendto(data2, (ip1, UDP_PORT_OUT))
             nm = 0
